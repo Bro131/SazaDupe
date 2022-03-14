d1978454697f5cb88bd67a10864f0d06202ae707
Bro131 <101485605+Bro131@users.noreply.github.com>
2022-03-14 14:24:13 +0700

    Create mainloadbf.lua

---
+++ mainloadbf.lua
@@ -0,0 +1,5 @@
+    if not game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled == false then
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/Bro131/Bro131/main/.lua")()
+        else
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/Bro131/Bro131/main/.lua")()
+       end
